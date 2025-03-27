ed30b9252e4a93ed5be310e81a9a7329904014ed
FabioMorrone <morronefabio04@gmail.com>
2025-03-27 17:31:30 +0100

    feat: add Bootstrap for styling and implement genre filter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,47 @@ import moviesList from "./data/movies"
 
 
 export default function App() {
+  const [genre, setGenre] = useState('')
 
-  const [movie, setMovie] = useState(moviesList)
-  const [filteredMovie, setFilteredMovie] = useState(movie)
 
-  useEffect(() => {
-    console.log(movie);
 
-  }, [])
+
 
 
   return (
     <>
+      <div className='container'>
 
-      {
-        moviesList.map((film) => <div> {film}</div>)
-      }
+        {
+          moviesList.map((film) =>
+            <div> {film.title}</div>
+          )
+        }
+        <div class="mb-3">
+          <label for="" class="form-label">Name</label>
+          <input onChange={e => setGenre(e.target.value)}
+            type="text"
+            class="form-control"
+            name=""
+            id=""
+            aria-describedby="helpId"
+            placeholder=""
+            value={genre}
+          />
+          <small id="helpId" class="form-text text-muted">Help text</small>
+        </div>
+
+        <select name="movie" id="movie-select">
+          <option value="">--Please choose an option--</option>
+          <option value="">Fantascienza</option>
+          <option value="">Thriller</option>
+          <option value="">Romantico</option>
+          <option value="">Azione</option>
+          <option value="">Fantascienza</option>
+          <option value="">Thriller</option>
+        </select>
+
+      </div>
 
 
     </>
